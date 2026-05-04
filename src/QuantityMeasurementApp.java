7803e1a277f954e8056534791fb36fe6ef92a1b7
kyouexe <sarimkhan112005@gmail.com>
2026-05-04 19:39:56 +0530

    UC1: Feet Measurement Equality

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,2 +1,60 @@
+package com.apps.quantitymeasurement;
+
+/**
+ * QuantityMeasurementApp – UC1: Feet measurement equality
+ *
+ * This class is responsible for checking the equality of two numerical values
+ * measured in feet in the Quantity Measurement Application.
+ */
 public class QuantityMeasurementApp {
+
+    // Inner class to represent Feet measurement
+    public static class Feet {
+        private final double value;
+
+        // Constructor
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        /**
+         * Override equals() method to compare two Feet objects based on value
+         */
+        @Override
+        public boolean equals(Object obj) {
+
+            // 1. Reference check
+            if (this == obj) {
+                return true;
+            }
+
+            // 2. Null check
+            if (obj == null) {
+                return false;
+            }
+
+            // 3. Type check
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+
+            // 4. Type casting
+            Feet other = (Feet) obj;
+
+            // 5. Value comparison using Double.compare()
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    // Main method to demonstrate Feet equality check
+    public static void main(String[] args) {
+
+        Feet value1 = new Feet(1.0);
+        Feet value2 = new Feet(1.0);
+
+        boolean result = value1.equals(value2);
+
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Output: Equal (" + result + ")");
+    }
 }
--- test/QuantityMeasurementAppTest.java
+++ test/QuantityMeasurementAppTest.java
@@ -1,2 +1,65 @@
+package com.apps.quantitymeasurement;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+
 public class QuantityMeasurementAppTest {
+
+    /**
+     * testEquality_SameValue
+     * Given two same values (1.0, 1.0)
+     * When compared
+     * Then should return true
+     */
+    @Test
+    public void testFeetEquality_SameValue() {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        assertTrue(f1.equals(f2), "Expected values to be equal");
+    }
+
+    /**
+     * testEquality_DifferentValue
+     */
+    @Test
+    public void testFeetEquality_DifferentValue() {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(2.0);
+
+        assertFalse(f1.equals(f2), "Expected values to be NOT equal");
+    }
+
+    /**
+     * testEquality_NullComparison
+     */
+    @Test
+    public void testFeetEquality_NullComparison() {
+        Feet f1 = new Feet(1.0);
+
+        assertFalse(f1.equals(null), "Expected comparison with null to be false");
+    }
+
+    /**
+     * testEquality_DifferentClass (Non-numeric input simulation)
+     */
+    @Test
+    public void testFeetEquality_DifferentClass() {
+        Feet f1 = new Feet(1.0);
+        String nonNumeric = "1.0";
+
+        assertFalse(f1.equals(nonNumeric), "Expected different type comparison to be false");
+    }
+
+    /**
+     * testEquality_SameReference (Reflexive property)
+     */
+    @Test
+    public void testFeetEquality_SameReference() {
+        Feet f1 = new Feet(1.0);
+
+        assertTrue(f1.equals(f1), "Expected object to be equal to itself");
+    }
 }
